d813e5f60788cde45a678021a852968635737c8b
joeyAddy <chatwithjohnjoseph@gmail.com>
2024-02-18 19:42:58 +0100

    Added rtl toggle for text editor

--- components/editor.tsx
+++ components/editor.tsx
@@ -8,20 +8,59 @@ import "react-quill/dist/quill.snow.css";
 interface EditorProps {
   onChange: (value: string) => void;
   value: string;
-};
+}
 
-export const Editor = ({
-  onChange,
-  value,
-}: EditorProps) => {
-  const ReactQuill = useMemo(() => dynamic(() => import("react-quill"), { ssr: false }), []);
+export const Editor = ({ onChange, value }: EditorProps) => {
+  const ReactQuill = useMemo(
+    () => dynamic(() => import("react-quill"), { ssr: false }),
+    []
+  );
 
+  const modules = {
+    toolbar: [
+      [{ header: [1, 2, false] }],
+      ["bold", "italic", "underline", "strike", "blockquote"],
+      [
+        { list: "ordered" },
+        { list: "bullet" },
+        { indent: "-1" },
+        { indent: "+1" },
+      ],
+      [{ direction: "rtl" }], // this is rtl support
+    ],
+  };
+
+  const formats = [
+    "header",
+    "font",
+    "background",
+    "color",
+    "code",
+    "size",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "list",
+    "bullet",
+    "indent",
+    "script",
+    "align",
+    "direction",
+    "link",
+    "image",
+    "code-block",
+    "formula",
+  ];
   return (
     <div className="bg-white">
       <ReactQuill
         theme="snow"
         value={value}
         onChange={onChange}
+        modules={modules}
+        formats={formats}
       />
     </div>
   );
