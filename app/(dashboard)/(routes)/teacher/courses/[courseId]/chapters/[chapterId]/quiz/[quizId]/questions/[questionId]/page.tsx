2101d095898e4005e8f42216022530020a52f03a
joeyAddy <chatwithjohnjoseph@gmail.com>
2024-02-18 18:53:45 +0100

    Made question answers compulsory

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/quiz/[quizId]/questions/[questionId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/quiz/[quizId]/questions/[questionId]/page.tsx
@@ -50,6 +50,7 @@ const QuestinoIdPage = async ({
   const requiredFields = [
     question.prompt,
     question.explanation,
+    question.answer !== null,
     question.options.length >= 2 && question.options.length <= 4,
   ];
 
--- app/(dashboard)/(routes)/teacher/courses/[courseId]/exam/[examId]/questions/[questionId]/_components/question-answer-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/exam/[examId]/questions/[questionId]/_components/question-answer-form.tsx
@@ -70,7 +70,7 @@ export const QuestionAnswerForm = ({
   return (
     <div className="mt-6 border bg-slate-100 rounded-md p-4">
       <div className="font-medium flex items-center justify-between">
-        Question answer (optional)
+        Question answer
         <Button onClick={toggleEdit} variant="ghost">
           {isEditing ? (
             <>Cancel</>
