59d761baaa9e32ac22032ec29bffedebbfd7fa05
abderrahmane <ilahalaha0987@gmail.com>
2024-04-06 17:11:07 +0100

    integrating the editor in the course description

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/description-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/description-form.tsx
@@ -20,6 +20,7 @@ import {
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { Textarea } from "@/components/ui/textarea";
+import { Editor } from "@/components/editor";
 
 interface DescriptionFormProps {
   initialData: Course;
@@ -97,11 +98,7 @@ export const DescriptionForm = ({
               render={({ field }) => (
                 <FormItem>
                   <FormControl>
-                    <Textarea
-                      disabled={isSubmitting}
-                      placeholder="e.g. 'This course is about...'"
-                      {...field}
-                    />
+                    <Editor {...field} />                                                              
                   </FormControl>
                   <FormMessage />
                 </FormItem>
